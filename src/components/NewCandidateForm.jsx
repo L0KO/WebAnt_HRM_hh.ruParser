021304bac41af4c407123ef85592bb5ed7bd2944
LOKO <AIM325@ya.ru>
2023-12-01 18:44:49 +0300

    Ended adding and deleting functions with connection to BackEnd

--- src/components/NewCandidateForm.jsx
+++ src/components/NewCandidateForm.jsx
@@ -1,29 +1,23 @@
 import "../../public/css/Form.scss"
 
-export function NewCandidateForm({ addCandidate, id }) {
+export function NewCandidateForm({ addCandidate, parseCandidate, parsedCandidat, id }) {
 
-  // function handleSubmit(e) {
-  //   // e.preventDefault()
-  //   const name = e.target.name.value
-  //   const post = e.target.post.value
-  //   const phone = e.target.phone.value
-  //   const email = e.target.email.value
-  //   addCandidate(name, post, phone, email)
-
-  //   e.target.name.value = ""
-  //   e.target.post.value = ""
-  //   e.target.phone.value = ""
-  //   e.target.email.value = ""
-  // }
+  function handleParse(e) {
+    e.preventDefault();
+    let dataToSend = {
+      resume_link: e.target.resume_link.value
+    };
+    parseCandidate(dataToSend)
+  }
 
   function handleFetch(e) {
-    e.preventDefault()
+    // e.preventDefault()
     const objectToSend = {
       id: id,
       state: {
         state:"Ожидание рассмотрения"
       },
-      name: e.target.name.value,
+      personal_name: e.target.name.value,
       contacts: {
         email: e.target.email.value,
         phone: e.target.phone.value
@@ -31,21 +25,21 @@ export function NewCandidateForm({ addCandidate, id }) {
       birth_date: e.target.birth_date.value,
       gender: e.target.gender.value,
       area: e.target.area.value,
-      post: e.target.post.value,
-      specialization: {
+      title: e.target.post.value,
+      specialization: [{
         name: e.target.specialization.value,
         profarea_name: "Специализации:"
-      },
+      }],
       salary: {
         amount: e.target.salary.value,
         currency: "₽ на руки"
       },
       education_level: e.target.education_level.value,
-      education: {
+      education: [{
         name: e.target.education_name.value,
         year: e.target.education_year.value,
         organization: e.target.education_organization.value
-      },
+      }],
       language: [{
         name: e.target.language_name.value,
         level: e.target.language_level.value
@@ -67,42 +61,25 @@ export function NewCandidateForm({ addCandidate, id }) {
 
   return (
     <>
-      {/* <form onSubmit={handleSubmit} method="post" action="http://localhost:3000/resume/">
-        <div className="input-container">
-        <div className="form__group field">
-          <input type="input" className="form__field" placeholder="Name" name="name" id='name' required />
-          <label htmlFor="name" className="form__label">Name</label>
-        </div>
-        <div className="form__group field">
-          <input type="input" className="form__field" placeholder="Post" name="post" id='post' required />
-          <label htmlFor="Post" className="form__label">Post</label>
-        </div>
-        <div className="form__group field">
-          <input type="phone" className="form__field" placeholder="Phone" name="phone" id='phone' required />
-          <label htmlFor="Phone" className="form__label">Phone</label>
-        </div>
-        <div className="form__group field">
-          <input type="email" className="form__field" placeholder="Email" name="email" id='email' required />
-          <label htmlFor="Email" className="form__label">Email</label>
-        </div>
-        </div>        
-        <button className="button-36" role="button" type="submit">Add new Candidate</button>
-      </form> */}
+      <form onSubmit={handleParse}>
+        <input type="url" name="resume_link" required /> 
+        <button className="button-36" role="button" type="submit">Parse Candidate</button>
+      </form>
       <form onSubmit={handleFetch}>
-        <input type="text" name="name" required />
-        <input type="email" name="email" required />
-        <input type="phone" name="phone" required />
-        <input type="date" name="birth_date" />
-        <select name="gender" id="gender">
+        <input type="text" name="name" defaultValue={parsedCandidat.personal_name} required />
+        <input type="email" name="email" defaultValue={parsedCandidat.contacts.email} required />
+        <input type="phone" name="phone" defaultValue={parsedCandidat.contacts.phone} required />
+        <input type="date" name="birth_date" defaultValue={parsedCandidat.birth_date} required />
+        <select name="gender" id="gender" defaultValue={parsedCandidat.gender} required>
           <option hidden value="">Ваш пол</option>
           <option value="Мужчина">Мужчина</option>
           <option value="Женщина">Женщина</option>
           <option value="Ламинат">Ламинат</option>
         </select>
-        <input type="text" name="specialization"/>
-        <input type="text" name="area"/>
-        <input type="text" name="post"/>
-        <input type="text" name="salary"/>
+        <input type="text" name="specialization" defaultValue={parsedCandidat.specialization.name}/>
+        <input type="text" name="area" defaultValue={parsedCandidat.area}/>
+        <input type="text" name="post"defaultValue={parsedCandidat.title}/>
+        <input type="text" name="salary" defaultValue={parsedCandidat.salary.amount}/>
         <select name="education_level" id="education_level">
           <option hidden value="">Ваш уровень образования</option>
           <option value="Среднее">Среднее</option>
@@ -114,10 +91,10 @@ export function NewCandidateForm({ addCandidate, id }) {
           <option value="Кандидат наук">Кандидат наук</option>
           <option value="Доктор наук">Доктор наук</option>
         </select>
-        <input type="text" name="education_name"/>
-        <input type="number" min="1900" max="2099" step="1" name="education_year"/>
-        <input type="text" name="education_organization"/>
-        <input type="text" name="language_name"/>
+        <input type="text" name="education_name" defaultValue={parsedCandidat.education[0].name}/>
+        <input type="number" min="1900" max="2099" step="1" name="education_year" defaultValue={parsedCandidat.education[0].year}/>
+        <input type="text" name="education_organization" defaultValue={parsedCandidat.education[0].organization}/>
+        <input type="text" name="language_name" defaultValue={parsedCandidat.language[0].name}/>
         <select name="language_level" id="language_level">
           <option hidden value="">Ваш уровень языка</option>
           <option value="A1 - Начальный">A1 - Начальный</option>
@@ -127,13 +104,13 @@ export function NewCandidateForm({ addCandidate, id }) {
           <option value="C1 - Продвинутый">C1 - Продвинутый</option>
           <option value="C2 - В совершенстве">C2 - В совершенстве</option>
         </select>
-        <input type="date" name="experience_start" />
-        <input type="date" name="experience_end" />
-        <input type="text" name="experience_position"/>
-        <input type="text" name="experience_organization"/>
-        <input type="text" name="experience_description"/>
-        <input type="text" name="skill_set"/>
-        <input type="text" name="skills"/>
+        <input type="date" name="experience_start" defaultValue={parsedCandidat.experience[0].start}/>
+        <input type="date" name="experience_end" defaultValue={parsedCandidat.experience[0].end}/>
+        <input type="text" name="experience_position" defaultValue={parsedCandidat.experience[0].position}/>
+        <input type="text" name="experience_organization" defaultValue={parsedCandidat.experience[0].organization}/>
+        <input type="text" name="experience_description" defaultValue={parsedCandidat.experience[0].description}/>
+        <input type="text" name="skill_set" defaultValue={parsedCandidat.skill_set[0]}/>
+        <input type="text" name="skills"defaultValue={parsedCandidat.skills}/>
         <button className="button-36" role="button" type="submit">Add new Candidate</button>
       </form>
     </>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,24 +5,66 @@ import { NewCandidateForm } from "../components/NewCandidateForm"
 export function Homepage() {
 
   const [candidat, setCandidat] = useState([])
+  const [parsedCandidat, setParsedCandidat] = useState({
+    id: 0,
+    state: {
+      state: "Ожидание рассмотрения"
+    },
+    personal_name: '',
+    contacts: {
+      email: '',
+      phone: ''
+    },
+    birth_date: '',
+    gender: '',
+    area: '',
+    title: '',
+    specialization: [{
+      name: '',
+      profarea_name: "Специализации:"
+    }],
+    salary: {
+      amount: '',
+      currency: "₽ на руки"
+    },
+    education_level: '',
+    education: [{
+      name: '',
+      year: '',
+      organization: ''
+    }],
+    language: [{
+      name: '',
+      level: ''
+    }],
+    experience: [{
+      end: '',
+      start: '',
+      position: '',
+      description: '',
+      organization: '',
+    }],
+    skill_set: [''],
+    skills: ''
+  })
 
   useEffect(() => {
     fetch('http://localhost:3000/resume/list/')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    const newCandidat = data.map(element => ({
-    id: element.id,
-    name: element.personal_name,
-    status: "",
-    post: element.title,
-    phone: element.contacts.phone,
-    email: element.contacts.email
-    }));
-    setCandidat(newCandidat);
-    })
-    .catch(error => console.log('Ошибка:', error));
-    }, []);
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const newCandidat = data.map(element => ({
+          id: element.id,
+          name: element.personal_name,
+          status: "",
+          post: element.title,
+          phone: element.contacts.phone,
+          email: element.contacts.email
+        }));
+        setCandidat(newCandidat);
+      })
+      .catch(error => console.log('Ошибка:', error));
+  }, []);
 
   function addCandidate(objectToSend) {
     let url = 'http://localhost:3000/resume/add/';
@@ -30,21 +72,45 @@ export function Homepage() {
     console.log(`data: ${data}`)
     console.log(data)
     fetch(url, {
-      method: 'POST', 
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data), 
+      body: JSON.stringify(data),
     })
   }
 
+  function parseCandidate(objectToSend) {
+    let url = 'http://localhost:3000/resume/add/parse';
+    let data = objectToSend;
+    console.log(`data: ${data}`)
+    console.log(data)
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        // let parsedData = JSON.parse(JSON.stringify(data));
+        // let parsedData = JSON.parse(JSON.stringify(data));
+        // console.log(parsedData);
+        setParsedCandidat(data)
+        console.log(parsedCandidat)
+      })
+  }
+
   function deleteCandidate(id) {
-    setCandidat(candidat.filter(candidate => candidate.id !== id))
+    // setCandidat(candidat.filter(candidate => candidate.id !== id))
+    fetch(`http://localhost:3000/resume/list/delete/${id}`, { method: 'POST' })
   }
 
   return (
     <>
-      <NewCandidateForm addCandidate={addCandidate} id={candidat.length}/>
+      <NewCandidateForm parseCandidate={parseCandidate} addCandidate={addCandidate} parsedCandidat={parsedCandidat} id={candidat.length} />
       <CandidatList candidat={candidat} deleteCandidate={deleteCandidate} />
     </>
   )
